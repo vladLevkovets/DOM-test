210c144c696767afcb95b490cdf6fc2099ecf90e
vladLevkovets <vl.vladisla@gmail.com>
2022-08-29 23:23:10 +0200

    sart visual

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const GAME=()=>{
-  const MAIN=[]
+  const MAIN=[{},{},{},{},{},{}]
   var tryes=10
   var gues=0
   var match=0
@@ -36,11 +36,11 @@ const GAME=()=>{
   
   for (tryes=10;tryes>0;tryes--){
     if (tryes>5){
-      gues=prompt(`enter password ,  ${tryes} attempts left`)
+      gues=prompt(`Enter password ,  ${tryes} attempts left`)
     }else if (tryes>1){
-        gues=prompt(`enter password , ONLY ${tryes} attempts left`)
+        gues=prompt(`Enter password , ONLY ${tryes} attempts left`)
     }else {
-        gues=prompt(`enter password , this is your last attempt`)
+        gues=prompt(`Enter password , this is your LAST attempt`)
       }
     if (gues.length!==4){
         alert ('There must be 4 digits')
@@ -49,6 +49,7 @@ const GAME=()=>{
     else {check(gues);
       if (res===1){
         alert (`CONGRATULATIONS !!! You are RIGHT - password is ${gues.join()}`)
+        return `YOU WON!!!`
       }else if(tryes===1) {
         alert (`SORRY !  You didn't guess the password!  GAME OVER !!!`)
         alert (`Password was ${MAIN}`)
@@ -57,7 +58,50 @@ const GAME=()=>{
       }
     }
   }
+  // VISUAL PART
+  const All=[{gues:"1234"},{gues:"5678"},{qgues:"5678"},{gues:"5678"}]
+  debugger
   
+  const renderAll=()=>{
+      createGame();
+      All.forEach(ele => {
+          // createScreen(ele);
+          createQ();
+          createA();
+          // createSpan();
+        }); 
+  }
+  
+  
+  
+  
+  const createGame=()=>{
+    const div=document.createElement("div");
+    div.setAttribute("class", "game");
+    div.setAttribute("id", "game")
+    main.appendChild(div);
+  //   createQ();
+  //   createA();
+    
+  }
+  
+  const createQ=()=>{
+      const Q=document.createElement("div")
+      Q.setAttribute("class", "gues");
+      Q.setAttribute("id", "")
+      // Q.innerText= ele.quest
+      document.getElementById("game").appendChild(Q);
+  }
+  
+  const createA=()=>{
+      const A=document.createElement("div")
+      A.setAttribute("class", "ans");
+      A.setAttribute("id", "")
+      // A.innerText= ele.quest
+      document.getElementById("game").appendChild(A);
+  }
+  
+
 }
   
  
